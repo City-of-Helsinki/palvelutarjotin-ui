54d6605d8242027422f77f8ee25bb34168392f3c
Jori Lindell <jori.lindell@gmail.com>
2020-06-13 00:29:55 +0300

    Add start.js file to eslintignore

--- src/start.js
+++ src/start.js
@@ -1,6 +1,3 @@
-/* eslint-disable no-native-reassign */
-/* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable @typescript-eslint/no-require-imports */
 require = require('esm')(module);
 const jsdom = require('jsdom');
 
