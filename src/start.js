58f3b2903ddac0d5b7dd65c7c0da4d44f8af835e
Jori Lindell <jori.lindell@gmail.com>
2020-06-12 22:12:44 +0300

    Inject styles to html head on server side

--- src/start.js
+++ src/start.js
@@ -1,6 +1,10 @@
 /* eslint-disable no-native-reassign */
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable @typescript-eslint/no-require-imports */
-// Set options as a parameter, environment variable, or rc file.
 require = require('esm')(module);
+const jsdom = require('jsdom');
+
+const document = new jsdom.JSDOM('<!DOCTYPE html>').window.document;
+global.document = document;
+
 module.exports = require('./server');
