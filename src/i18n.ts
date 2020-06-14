86604a0874ba2992e27e0fd1f6fd7b8a7a06d5c0
Jori Lindell <jori.lindell@gmail.com>
2020-06-14 17:58:04 +0300

    Add healthiness routes to server

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,6 +2,7 @@ import NextI18Next from 'next-i18next';
 
 const NextI18NextInstance = new NextI18Next({
   defaultLanguage: 'fi',
+  ignoreRoutes: ['/healthz', '/readiness'],
   localeSubpaths: {
     en: 'en',
     fi: 'fi',
--- src/server.js
+++ src/server.js
@@ -8,14 +8,44 @@ const port = process.env.PORT || 3000;
 const app = next({ dev: process.env.NODE_ENV !== 'production' });
 const handle = app.getRequestHandler();
 
+const RESPONSES = {
+  OK: 'OK',
+  SERVER_IS_NOT_READY: 'SERVER_IS_NOT_READY',
+};
+
+let serverIsReady = false;
+
+const signalReady = () => {
+  serverIsReady = true;
+};
+
+const checkIsServerReady = (response) => {
+  if (serverIsReady) {
+    response.send(RESPONSES.OK);
+  } else {
+    response.status(500).send(RESPONSES.SERVER_IS_NOT_READY);
+  }
+};
+
 (async () => {
   await app.prepare();
   const server = express();
 
   await nextI18next.initPromise;
+
   server.use(nextI18NextMiddleware(nextI18next));
 
+  server.get('/healthz', (req, res) => {
+    checkIsServerReady(res);
+  });
+
+  server.get('/readiness', (req, res) => {
+    checkIsServerReady(res);
+  });
+
   server.get('*', (req, res) => handle(req, res));
+
   await server.listen(port);
+  signalReady();
   console.log(`> Ready on http://localhost:${port}`); // eslint-disable-line no-console
 })();
