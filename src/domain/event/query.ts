ae956f6815b5aa26e7d44e2754fcf33611b4e191
Jori Lindell <jori.lindell@gmail.com>
2020-06-14 21:09:29 +0300

    Generate GraphQl types to fetch event list data

---
+++ src/domain/event/query.ts
@@ -0,0 +1,73 @@
+import gql from 'graphql-tag';
+
+export const QUERY_EVENT = gql`
+  fragment pEventFields on PalvelutarjotinEventNode {
+    id
+    duration
+    enrolmentEndDays
+    enrolmentStart
+    neededOccurrences
+    occurrences {
+      edges {
+        node {
+          ...occurrenceFields
+        }
+      }
+    }
+  }
+
+  fragment localisedFields on LocalisedObject {
+    en
+    fi
+    sv
+  }
+
+  fragment eventFields on Event {
+    id
+    internalId
+    name {
+      ...localisedFields
+    }
+    shortDescription {
+      ...localisedFields
+    }
+    description {
+      ...localisedFields
+    }
+    images {
+      ...imageFields
+    }
+    infoUrl {
+      ...localisedFields
+    }
+    pEvent {
+      ...pEventFields
+    }
+    inLanguage {
+      id
+      internalId
+      name {
+        ...localisedFields
+      }
+    }
+    audience {
+      ...keywordFields
+    }
+    keywords {
+      ...keywordFields
+    }
+    location {
+      ...placeFields
+    }
+    venue {
+      ...venueFields
+    }
+    startTime
+  }
+
+  query Event($id: ID!, $include: [String]) {
+    event(id: $id, include: $include) {
+      ...eventFields
+    }
+  }
+`;
--- src/domain/events/EventsPage.tsx
+++ src/domain/events/EventsPage.tsx
@@ -2,10 +2,12 @@ import Head from 'next/head';
 import React, { ReactElement } from 'react';
 import { useTranslation } from 'react-i18next';
 
+import { useEventsQuery } from '../../generated/graphql';
 import Container from '../app/layout/Container';
 
 const EventsPage = (): ReactElement => {
   const { t } = useTranslation();
+  const { data: eventsData } = useEventsQuery({ ssr: false });
 
   return (
     <div>
