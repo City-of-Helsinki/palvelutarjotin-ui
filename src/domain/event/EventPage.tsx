0e5781bc994b02a506cde00af90c33c357d1e2d0
Jori Lindell <jori.lindell@gmail.com>
2020-07-01 15:28:58 +0300

    Fetch event data and show not found page if event doesn't exist

--- src/domain/event/EventPage.tsx
+++ src/domain/event/EventPage.tsx
@@ -1,14 +1,39 @@
+import { useRouter } from 'next/router';
 import React, { ReactElement } from 'react';
 import { useTranslation } from 'react-i18next';
 
+import { useEventQuery } from '../../generated/graphql';
+import useLocale from '../../hooks/useLocale';
+import getLocalisedString from '../../utils/getLocalisedString';
 import Container from '../app/layout/Container';
 import PageWrapper from '../app/layout/PageWrapper';
+import NotFoundPage from '../notFoundPage/NotFoundPage';
+
+interface Params {
+  id: string;
+}
 
 const EventPage = (): ReactElement => {
   const { t } = useTranslation();
+  const locale = useLocale();
+  const { query } = useRouter();
+  const { id } = query;
+
+  const { data: eventData } = useEventQuery({
+    variables: { id: id as string },
+  });
+
+  const name = getLocalisedString(eventData?.event?.name || {}, locale);
+
   return (
-    <PageWrapper title={t('event:pageTitle')}>
-      <Container>TODO: EVENT PAGE</Container>
+    <PageWrapper title={name || t('event:pageTitle')}>
+      {eventData?.event ? (
+        <Container>
+          <h1>{name}</h1>
+        </Container>
+      ) : (
+        <NotFoundPage />
+      )}
     </PageWrapper>
   );
 };
