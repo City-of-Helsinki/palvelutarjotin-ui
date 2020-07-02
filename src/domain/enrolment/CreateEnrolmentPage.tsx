32efe000e2e85e00f73e35dd021672098f2e06c1
Jori Lindell <jori.lindell@gmail.com>
2020-07-02 21:34:18 +0300

    Add back button to create enrolment page

--- src/domain/enrolment/CreateEnrolmentPage.tsx
+++ src/domain/enrolment/CreateEnrolmentPage.tsx
@@ -1,14 +1,16 @@
+import { Button, IconArrowLeft } from 'hds-react';
 import { useRouter } from 'next/router';
 import React, { ReactElement } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import LoadingSpinner from '../../common/components/loadingSpinner/LoadingSpinner';
 import { useEventQuery } from '../../generated/graphql';
-import useLocale from '../../hooks/useLocale';
-import getLocalisedString from '../../utils/getLocalisedString';
+import { Router } from '../../i18n';
 import Container from '../app/layout/Container';
 import PageWrapper from '../app/layout/PageWrapper';
+import { ROUTES } from '../app/routes/constants';
 import NotFoundPage from '../notFoundPage/NotFoundPage';
+import styles from './enrolmentPage.module.scss';
 
 const CreateEnrolmentPage = (): ReactElement => {
   const { t } = useTranslation();
@@ -20,13 +22,29 @@ const CreateEnrolmentPage = (): ReactElement => {
     variables: { id: eventId as string },
   });
 
+  const goToEventPage = () => {
+    Router.push(ROUTES.EVENT_DETAILS.replace(':id', eventId as string));
+  };
+
   return (
     <PageWrapper title={t('enrolment:pageTitle')}>
       <LoadingSpinner isLoading={loading}>
         {eventData?.event ? (
-          <Container>
-            <h1>{t('enrolment:title')}</h1>
-          </Container>
+          <div className={styles.enrolmentPage}>
+            <Container>
+              <div className={styles.buttonWrapper}>
+                <Button
+                  iconLeft={<IconArrowLeft />}
+                  onClick={goToEventPage}
+                  variant="secondary"
+                >
+                  {t('enrolment:buttonBack')}
+                </Button>
+              </div>
+
+              <h1>{t('enrolment:title')}</h1>
+            </Container>
+          </div>
         ) : (
           <NotFoundPage />
         )}
