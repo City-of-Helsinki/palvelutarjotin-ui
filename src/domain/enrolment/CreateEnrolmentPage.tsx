beee7d228e93d9f80211ca847968f842d8d2be2d
Jori Lindell <jori.lindell@gmail.com>
2020-07-03 13:31:09 +0300

    Occurrrence table without any filttering

--- src/domain/enrolment/CreateEnrolmentPage.tsx
+++ src/domain/enrolment/CreateEnrolmentPage.tsx
@@ -4,7 +4,10 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 import LoadingSpinner from '../../common/components/loadingSpinner/LoadingSpinner';
-import { useEventQuery } from '../../generated/graphql';
+import {
+  useEventQuery,
+  OccurrenceFieldsFragment,
+} from '../../generated/graphql';
 import { Router } from '../../i18n';
 import Container from '../app/layout/Container';
 import PageWrapper from '../app/layout/PageWrapper';
@@ -12,6 +15,7 @@ import { ROUTES } from '../app/routes/constants';
 import NotFoundPage from '../notFoundPage/NotFoundPage';
 import styles from './enrolmentPage.module.scss';
 import EventInfo from './eventInfo/EventInfo';
+import OccurrenceTable from './occurrenceTable/OccurrenceTable';
 
 const CreateEnrolmentPage: React.FC = () => {
   const { t } = useTranslation();
@@ -28,6 +32,11 @@ const CreateEnrolmentPage: React.FC = () => {
   };
 
   const event = eventData?.event;
+  const eventLocationId = event?.location?.id || '';
+  const occurrences: OccurrenceFieldsFragment[] =
+    event?.pEvent?.occurrences.edges.map(
+      (edge) => edge?.node as OccurrenceFieldsFragment
+    ) || [];
 
   return (
     <PageWrapper title={t('enrolment:pageTitle')}>
@@ -48,6 +57,10 @@ const CreateEnrolmentPage: React.FC = () => {
               <h1>{t('enrolment:title')}</h1>
               <div className={styles.divider} />
               <EventInfo event={event} />
+              <OccurrenceTable
+                eventLocationId={eventLocationId}
+                occurrences={occurrences}
+              />
             </Container>
           </div>
         ) : (
