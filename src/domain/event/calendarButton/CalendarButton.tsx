34e99e349bf9d2dac031b0f95916714668cbf3bd
Santeri Rusila <santeri.rusila@sarmadigital.com>
2020-07-14 15:14:51 +0300

    Ics download to occurrence item

---
+++ src/domain/event/calendarButton/CalendarButton.tsx
@@ -0,0 +1,83 @@
+import * as Sentry from '@sentry/browser';
+import { saveAs } from 'file-saver';
+import { Button, IconCalendar } from 'hds-react';
+import { createEvent, EventAttributes } from 'ics';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+import {
+  OccurrenceFieldsFragment,
+  EventFieldsFragment,
+  usePlaceQuery,
+} from '../../../generated/graphql';
+import useLocale from '../../../hooks/useLocale';
+import getDateArray from '../../../utils/getDateArray';
+import getDomain from '../../../utils/getDomain';
+import getLocalisedString from '../../../utils/getLocalisedString';
+import { ROUTES } from '../../app/routes/constants';
+import { getPlaceFields } from '../../place/utils';
+
+export interface CalendarButtonProps {
+  event: EventFieldsFragment;
+  occurrence: OccurrenceFieldsFragment;
+}
+
+const CalendarButton: React.FC<CalendarButtonProps> = ({
+  event,
+  occurrence,
+}) => {
+  const { t } = useTranslation();
+  const locale = useLocale();
+  const placeId = occurrence.placeId || event.location?.id || '';
+
+  const { data } = usePlaceQuery({
+    variables: { id: placeId },
+  });
+
+  const { streetAddress, addressLocality, name: locationName } = getPlaceFields(
+    data?.place,
+    locale
+  );
+
+  const downloadIcsFile = () => {
+    if (occurrence.startTime && event.id) {
+      const domain = getDomain();
+      const icsEvent: EventAttributes = {
+        description: t('event:info.textCalendarLinkDescription', {
+          description: getLocalisedString(event.shortDescription || {}, locale),
+          link: `${domain}/${locale}${ROUTES.EVENT.replace(':id', event.id)}`,
+        }),
+        end: occurrence.endTime
+          ? getDateArray(occurrence.endTime)
+          : getDateArray(occurrence.startTime),
+        location: [streetAddress, locationName, addressLocality]
+          .filter((e) => e)
+          .join(', '),
+        productId: domain,
+        start: getDateArray(occurrence.startTime),
+        startOutputType: 'local',
+        title: getLocalisedString(event.name || {}, locale),
+      };
+      createEvent(icsEvent, (error: Error | undefined, value: string) => {
+        if (error) {
+          Sentry.captureException(error);
+        } else {
+          const blob = new Blob([value], { type: 'text/calendar' });
+          saveAs(blob, `event_${event.id?.replace(/:/g, '')}.ics`);
+        }
+      });
+    }
+  };
+
+  return (
+    <Button
+      onClick={downloadIcsFile}
+      iconLeft={<IconCalendar />}
+      variant="supplementary"
+    >
+      {t('event:occurrenceList.downloadToCalendar')}
+    </Button>
+  );
+};
+
+export default CalendarButton;
