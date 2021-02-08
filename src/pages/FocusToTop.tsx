2b041a2f108d8dab436051029b787b67dd59c4a9
Santeri Rusila <santtu_1993@hotmail.com>
2021-02-08 17:53:15 +0200

    Fic focus to top funcitonality: add new FocusToTop component

---
+++ src/pages/FocusToTop.tsx
@@ -0,0 +1,21 @@
+import { Router } from 'next/router';
+import * as React from 'react';
+
+const FocusToTop: React.FC = () => {
+  const focusRef = React.useRef<HTMLDivElement>(null);
+
+  const handleFocus = () => {
+    focusRef.current?.focus();
+  };
+
+  React.useEffect(() => {
+    Router.events.on('routeChangeComplete', handleFocus);
+    return () => {
+      Router.events.off('routeChangeComplete', handleFocus);
+    };
+  }, []);
+
+  return <div ref={focusRef} tabIndex={-1} />;
+};
+
+export default FocusToTop;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,6 @@ import { NormalizedCacheObject } from 'apollo-cache-inmemory';
 import { ApolloClient } from 'apollo-client';
 import flow from 'lodash/flow';
 import App from 'next/app';
-import { Router } from 'next/router';
 import React, { ErrorInfo } from 'react';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
@@ -14,6 +13,7 @@ import withApollo from '../domain/app/apollo/configureApollo';
 import PageLayout from '../domain/app/layout/PageLayout';
 import { store } from '../domain/app/store';
 import { appWithTranslation, i18n } from '../i18n';
+import FocusToTop from './FocusToTop';
 
 interface Props {
   apollo: ApolloClient<NormalizedCacheObject>;
@@ -36,8 +36,6 @@ class MyApp extends App<Props> {
         html.setAttribute('lang', lang);
       }
     });
-
-    this.setTabIndexOnRouteChange();
   }
 
   componentDidCatch(error: Error, errorInfo: ErrorInfo) {
@@ -50,24 +48,13 @@ class MyApp extends App<Props> {
     super.componentDidCatch(error, errorInfo);
   }
 
-  // next focuses body between route changes if it has tabIndex=-1
-  setTabIndexOnRouteChange() {
-    // see: https://github.com/vercel/next.js/issues/7681#issuecomment-603032899
-    Router.events.on('routeChangeStart', () => {
-      document.body.setAttribute('tabIndex', '-1');
-    });
-
-    document.body.addEventListener('blur', () => {
-      document.body.removeAttribute('tabIndex');
-    });
-  }
-
   render() {
     const { apollo, Component, pageProps } = this.props;
 
     return (
       <ApolloProvider client={apollo}>
         <Provider store={store}>
+          <FocusToTop />
           <PageLayout {...pageProps} namespacesRequired={['common', 'footer']}>
             <Component {...pageProps} />
           </PageLayout>
