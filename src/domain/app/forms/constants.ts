4150484384b18d7486b1d8a1b91275f3e7e45fb4
Niko Lindroos <niko.j.makela@gmail.com>
2021-02-17 11:10:37 +0200

    Improved the max groups size validations of the enrolment form.
    PT-772 Added new validation messages and refactored the live validation of children and adult fields. They now have a validation against max group size as a single and together.

--- src/domain/app/forms/constants.ts
+++ src/domain/app/forms/constants.ts
@@ -17,4 +17,6 @@ export enum VALIDATION_MESSAGE_KEYS {
   TIME_MAX = 'form:validation.time.min',
   TIME_REQUIRED = 'form:validation.time.required',
   STUDYGROUP_MAX_WITH_CHILDREN = 'form:validation.studygroup.adultmaxwithchildren',
+  STUDYGROUP_MAX_WITH_ADULTS = 'form:validation.studygroup.childrenmaxwithadults',
+  STUDYGROUP_MAX_CHILDREN_WITH_ADULTS = 'form:validation.studygroup.childrenandadultmax',
 }
