39af0ba2b5d3732724cfc5363a965a10c8171f79
Santeri Rusila <santtu_1993@hotmail.com>
2021-11-11 09:38:35 +0200

    chore: add FormikPersist feature toggle

--- src/utils/featureFlags.ts
+++ src/utils/featureFlags.ts
@@ -1,9 +1,11 @@
 export type FeatureFlags = {
   HEADLESS_CMS: boolean;
+  FORMIK_PERSIST: boolean;
 };
 
 export const getFeatureFlags = (): FeatureFlags => ({
   HEADLESS_CMS: process.env.NEXT_PUBLIC_HEADLESS_CMS_ENABLED === 'true',
+  FORMIK_PERSIST: process.env.NEXT_PUBLIC_FORMIK_PERSIST === 'true',
 });
 
 export const isFeatureEnabled = (feature: keyof FeatureFlags): boolean =>
