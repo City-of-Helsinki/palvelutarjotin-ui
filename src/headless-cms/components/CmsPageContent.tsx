df714f4b56b2fcded0d0a2f3bb258bc4efbf16b2
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2022-01-19 04:39:08 +0200

    feat(sidebar): use sidebar layout for all content

--- src/headless-cms/components/CmsPageContent.tsx
+++ src/headless-cms/components/CmsPageContent.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
 
 import { PageFieldsFragment } from '../../generated/graphql-cms';
-import CmsOneColumnLayout from './CmsOneColumnLayout';
 import CmsSidebarLayout from './CmsSidebarLayout';
 
 const CmsPageContent: React.FC<{ page: PageFieldsFragment | undefined }> = ({
   page,
 }) => {
-  const sidebarContents = page?.sidebar || [];
-
-  if (sidebarContents.length > 0) {
-    return <CmsSidebarLayout page={page} />;
-  }
-
-  return <CmsOneColumnLayout page={page} />;
+  return <CmsSidebarLayout page={page} />;
 };
 
 export default CmsPageContent;
