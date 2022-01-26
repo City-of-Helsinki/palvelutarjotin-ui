d0d4cd7fb86a37d98eb502f23190a5b39d12e94c
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2022-01-26 12:38:11 +0200

    refactor(sidebar): share post item render logic

--- src/headless-cms/components/cmsSidebarContent/CmsSidebarContent.tsx
+++ src/headless-cms/components/cmsSidebarContent/CmsSidebarContent.tsx
@@ -29,53 +29,15 @@ const CmsSidebarContent: React.FC<Props> = ({ content }) => {
         }
 
         if (item?.__typename === 'LayoutPages') {
-          return item?.pages?.map((page) => {
-            const title = page?.title;
-            const uri = page?.uri;
-
-            if (!title || !uri) {
-              return null;
-            }
-
-            return (
-              <li key={page?.id}>
-                <CmsSidebarContentCard
-                  title={title}
-                  // Remove language parameter uri because it's in the incorrect
-                  // position
-                  url={getCmsPath(stripLocaleFromUri(uri))}
-                  image={page?.featuredImage?.node?.mediaItemUrl || undefined}
-                  imageAlt={page?.featuredImage?.node?.altText || undefined}
-                />
-              </li>
-            );
-          });
+          return item?.pages?.map((page) => (
+            <PostListItem key={page?.id} {...page} />
+          ));
         }
 
         if (item?.__typename === 'LayoutArticles') {
-          return item?.articles?.map((article) => {
-            const title = article?.title;
-            const uri = article?.uri;
-
-            if (!title || !uri) {
-              return null;
-            }
-
-            return (
-              <li key={article?.id}>
-                <CmsSidebarContentCard
-                  title={title}
-                  // Remove language parameter uri because it's in the incorrect
-                  // position
-                  url={getCmsPath(stripLocaleFromUri(uri))}
-                  image={
-                    article?.featuredImage?.node?.mediaItemUrl || undefined
-                  }
-                  imageAlt={article?.featuredImage?.node?.altText || undefined}
-                />
-              </li>
-            );
-          });
+          return item?.articles?.map((article) => (
+            <PostListItem key={article?.id} {...article} />
+          ));
         }
 
         return null;
@@ -85,3 +47,34 @@ const CmsSidebarContent: React.FC<Props> = ({ content }) => {
 };
 
 export default CmsSidebarContent;
+
+type PostListItemProps = {
+  id?: string | null;
+  title?: string | null;
+  uri?: string | null;
+  featuredImage?: {
+    node?: {
+      mediaItemUrl?: string | null;
+      altText?: string | null;
+    } | null;
+  } | null;
+};
+
+const PostListItem = ({ id, title, uri, featuredImage }: PostListItemProps) => {
+  if (!title || !uri) {
+    return null;
+  }
+
+  return (
+    <li key={id}>
+      <CmsSidebarContentCard
+        title={title}
+        // Remove language parameter uri because it's in the incorrect
+        // position
+        url={getCmsPath(stripLocaleFromUri(uri))}
+        image={featuredImage?.node?.mediaItemUrl || undefined}
+        imageAlt={featuredImage?.node?.altText || undefined}
+      />
+    </li>
+  );
+};
