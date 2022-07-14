5ab2d129f95692c7eb7c552e222e3d0666b5474c
Tero Virtanen <tero.virtanen@mavericks.fi>
2022-07-14 08:26:41 +0300

    Load test for palvelutarjotin.test.kuva.hel.ninja DEVOPS-428

--- k6LoadTests.js
+++ k6LoadTests.js
@@ -5,6 +5,7 @@ import http from 'k6/http';
 export const options = {
   duration: '10m',
   vus: 20,
+  //  vus: 1,
   thresholds: {
     //avg is around ?800ms? on https://palvelutarjotin.test.kuva.hel.ninja
     http_req_duration: ['p(95)<5000'],
