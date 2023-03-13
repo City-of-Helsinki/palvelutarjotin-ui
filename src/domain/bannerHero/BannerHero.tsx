30c14f0d3b257e248a8704bad1599a698ae4d513
Kari Salminen <kari.salminen@anders.com>
2023-03-13 18:09:32 +0200

    fix: fix banner hero layout accessibility issue 4.1
    refs PT-1606

--- src/domain/bannerHero/BannerHero.tsx
+++ src/domain/bannerHero/BannerHero.tsx
@@ -1,3 +1,4 @@
+import { Koros } from 'hds-react';
 import { useTranslation } from 'next-i18next';
 import React from 'react';
 
@@ -8,18 +9,29 @@ import Container from '../app/layout/Container';
 const BannerHero: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
   const { t } = useTranslation();
   return (
-    <div
-      className={styles.bannerHeroContainer}
-      test-id="banner-hero-image"
-      style={{
-        backgroundImage: `url(${BANNER_IMAGE})`,
-      }}
-    >
-      <Container>
-        <h1>{t('common:bannerHero.title')}</h1>
-      </Container>
-      {children}
-    </div>
+    <>
+      <div className={styles.bannerHeroUpperText}>
+        <Container>
+          <h1>{t('common:bannerHero.topTitle')}</h1>
+          <h2>{t('common:bannerHero.middleTitle')}</h2>
+          <h3>{t('common:bannerHero.bottomTitle')}</h3>
+        </Container>
+      </div>
+      <div className={styles.bannerHeroContainer}>
+        <div
+          className={styles.bannerHeroImage}
+          test-id="banner-hero-image"
+          style={{
+            backgroundImage: `url(${BANNER_IMAGE})`,
+          }}
+        >
+          <Koros className={styles.korosTop} />
+          <div className={styles.bannerHeroSpacer} />
+          <Koros className={styles.korosBottom} />
+        </div>
+        <div className={styles.bannerHeroOverlay}>{children}</div>
+      </div>
+    </>
   );
 };
 
