244b4d36fbef5e69a069053631f16c86be3d7227
Tero Virtanen <tero.virtanen@mavericks.fi>
2023-07-18 13:08:55 +0300

    fix: release and build information PT-1652

---
+++ src/pages/api/__tests__/readiness.test.ts
@@ -0,0 +1,25 @@
+import handler from '../readiness';
+
+test('readiness response', async () => {
+  const req = {};
+  const json = jest.fn();
+  const status = jest.fn(() => {
+    return {
+      json,
+    };
+  });
+  const res = {
+    status,
+  };
+
+  handler(req, res);
+  const res_json = json.mock.calls[0][0];
+
+  // console.log(res_json);
+
+  expect(res_json.status).toEqual('ok');
+  expect(res_json.release);
+  expect(res_json.packageVersion);
+  expect(res_json.commitHash);
+  expect(res_json.buildTime);
+});
