aedcf5367ee6b901c4be8f20f17cc5cfbfad8f2d
Tero Virtanen <tero.virtanen@mavericks.fi>
2023-07-24 07:59:08 +0300

    chore: remove useless commented code

--- src/pages/api/__tests__/version.test.ts
+++ src/pages/api/__tests__/version.test.ts
@@ -15,8 +15,6 @@ test('version response', async () => {
   handler(req, res);
   const res_json = json.mock.calls[0][0];
 
-  // console.log(res_json);
-
   expect(res_json.status).toEqual('ok');
   expect(res_json.release);
   expect(res_json.packageVersion);
