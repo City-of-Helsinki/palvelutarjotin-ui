a5680709588151aef137b1ffe6631a868d4c17bf
Tero Virtanen <105207219+terovirtanen@users.noreply.github.com>
2023-07-24 15:08:39 +0300

    fix: version, release and build information PT-1652 (#284)

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const packageJson = require('./package.json');
 
 const { i18n } = require('./next-i18next.config');
 
@@ -14,5 +15,10 @@ module.exports = {
       'hkih.stage.geniem',
       'kultus.hkih.stage.geniem.io',
     ],
+  },
+  env: {
+    APP_NAME: packageJson.name,
+    APP_VERSION: packageJson.version,
+    BUILD_TIME: new Date().toISOString(),
   },
 };
---
+++ src/pages/api/__tests__/version.test.ts
@@ -0,0 +1,23 @@
+import handler from '../version';
+
+test('version response', async () => {
+  const req = {};
+  const json = jest.fn();
+  const status = jest.fn(() => {
+    return {
+      json,
+    };
+  });
+  const res = {
+    status,
+  };
+
+  handler(req, res);
+  const res_json = json.mock.calls[0][0];
+
+  expect(res_json.status).toEqual('ok');
+  expect(res_json.release);
+  expect(res_json.packageVersion);
+  expect(res_json.commitHash);
+  expect(res_json.buildTime);
+});
