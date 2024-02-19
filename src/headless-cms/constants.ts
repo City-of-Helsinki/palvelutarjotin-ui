dfc50d7dbceabb03d4073b37859a1524f89f5e5a
Ivan Melnik <melnik.job@gmail.com>
2024-02-19 20:00:44 +0200

    fix: header refactoring

--- src/headless-cms/constants.ts
+++ src/headless-cms/constants.ts
@@ -1,3 +1,5 @@
+//TODO: still used for the breadcrumb but contradicts with the current Navigation logic with localized menus?
+
 export enum MENU_NAME {
   Header = 'Palvelutarjotin-UI Header',
   Footer = 'Palvelutarjotin-UI Footer',
