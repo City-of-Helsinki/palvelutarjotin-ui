c8adc2cee433a213fe4326836b2edf49bc2e80ca
Ivan Melnik <melnik.job@gmail.com>
2024-04-15 14:22:30 +0300

    fix: event hero background

--- src/domain/event/eventImage/EventImage.tsx
+++ src/domain/event/eventImage/EventImage.tsx
@@ -17,12 +17,10 @@ const EventImage: React.FC<EventImageProps> = ({
   const { t } = useTranslation();
 
   return (
-    <div className={styles.imageContainer}>
-      <img
-        className={styles.eventImage}
-        src={imageUrl}
-        alt={imageAltText ?? undefined}
-      />
+    <div
+      className={styles.imageContainer}
+      style={{ backgroundImage: `url('${imageUrl}')` }}
+    >
       {photographerName && (
         <div className={styles.photographerText}>
           {t('event:photographerText', {
