e7132ee216fc3aec0de936371b3e62dd5e823ce5
Niko Lindroos <niko.j.makela@gmail.com>
2025-03-10 17:10:36 +0200

    refactor: split _app code to multiple files
    PT-1795

---
+++ src/domain/app/Center.tsx
@@ -0,0 +1,17 @@
+const Center: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
+  return (
+    <div
+      style={{
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Center;
