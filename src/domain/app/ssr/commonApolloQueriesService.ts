0b18fd6bbf2ecc5eed057d85c69abf0677b43fc3
Niko Lindroos <niko.j.makela@gmail.com>
2025-03-14 14:29:21 +0200

    feat: nextjs on demand revalidation API
    PT-1795

--- src/domain/app/ssr/commonApolloQueriesService.ts
+++ src/domain/app/ssr/commonApolloQueriesService.ts
@@ -1,15 +1,26 @@
 import { ApolloClient, NormalizedCacheObject } from '@apollo/client';
+import { ArticleType, PageType } from 'react-helsinki-headless-cms';
 import {
   MenuDocument,
   MenuQuery,
   MenuQueryVariables,
+  PagesDocument,
+  PagesQuery,
+  PagesQueryVariables,
+  PostsDocument,
+  PostsQuery,
+  PostsQueryVariables,
 } from 'react-helsinki-headless-cms/apollo';
 
+import { PageUriInfo } from './types';
 import {
   DEFAULT_HEADER_MENU_NAME,
   DEFAULT_FOOTER_MENU_NAME,
   SUPPORTED_LANGUAGES,
 } from '../../../constants';
+
+export const ARTICLES_AMOUNT_LIMIT = 100;
+export const PAGES_AMOUNT_LIMIT = 100;
 
 /**
  * Service class for executing Apollo queries related to CMS data.
@@ -64,5 +75,72 @@ export class CommonApolloQueriesService {
         menuIdentifiersOnly: true,
       },
     });
+  }
+
+  async getAllPages(): Promise<PageUriInfo[]> {
+    const pageInfos: PageUriInfo[] = [];
+    const { data: pagesData } = await this.cmsApolloClient.query<
+      PagesQuery,
+      PagesQueryVariables
+    >({
+      query: PagesDocument,
+      variables: {
+        first: PAGES_AMOUNT_LIMIT,
+      },
+    });
+    pagesData.pages?.edges?.forEach((edge) =>
+      this.addNodesToPageInfosArray(pageInfos, edge?.node as PageType)
+    );
+    return pageInfos;
+  }
+
+  async getAllArticles(): Promise<PageUriInfo[]> {
+    const pageInfos: PageUriInfo[] = [];
+    const { data: articlesData } = await this.cmsApolloClient.query<
+      PostsQuery,
+      PostsQueryVariables
+    >({
+      query: PostsDocument,
+      variables: {
+        first: ARTICLES_AMOUNT_LIMIT,
+      },
+    });
+    articlesData.posts?.edges?.forEach((edge) =>
+      this.addNodesToPageInfosArray(pageInfos, edge?.node as ArticleType)
+    );
+    return pageInfos;
+  }
+
+  private addNodesToPageInfosArray(
+    pageInfos: PageUriInfo[],
+    node: PageType | ArticleType
+  ) {
+    if (node && node.uri && node.slug && node.language?.code) {
+      pageInfos.push({
+        uri: node.uri,
+        locale: node.language.code.toLowerCase(),
+        slug: node.slug,
+      });
+      // NOTE: HCRC-build sometimes fails - this type should not be needed.
+      // : PageType['translations']
+      node.translations?.forEach((translation) => {
+        if (
+          translation?.uri &&
+          translation.slug &&
+          translation.language?.code
+        ) {
+          const {
+            uri,
+            slug,
+            language: { code },
+          } = translation;
+          pageInfos.push({
+            uri,
+            slug,
+            locale: code.toLocaleLowerCase(),
+          });
+        }
+      });
+    }
   }
 }
---
+++ src/domain/app/ssr/types.ts
@@ -0,0 +1,1 @@
+export type PageUriInfo = { uri: string; slug: string; locale: string };
