df24a4c1453002efd4099e554dd8b8735c5c0b0c
Kari Salminen <kari.salminen@anders.com>
2025-03-28 13:52:02 +0200

    chore: update jsdom to latest, remove @testing-library/react-hooks
    also:
     - make use of @testing-library/react more to get rid of warnings when
       running tests
     - remove unused @testing-library/react-hooks
     - run "yarn upgrade" to update yarn.lock
    
    refs PT-1884

--- src/domain/event/occurrences/__tests__/useDateFiltering.test.tsx
+++ src/domain/event/occurrences/__tests__/useDateFiltering.test.tsx
@@ -1,4 +1,4 @@
-import { act, renderHook } from '@testing-library/react-hooks';
+import { act, renderHook } from '@testing-library/react';
 import { format } from 'date-fns';
 
 import { fakeOccurrence } from '../../../../utils/mockDataUtils';
