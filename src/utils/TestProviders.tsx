233197fba6fbe1fade2cd6b6e257caa37acb1ee9
Kari Salminen <kari.salminen@anders.com>
2025-04-14 13:36:59 +0300

    chore: return void when void is expected in TestProviders functions
    "Promise-returning function provided to property where a void return was
    expected. Promises should not be misused":
    https://rules.sonarsource.com/typescript/RSPEC-6544/
    
    refs PT-1911

--- src/utils/TestProviders.tsx
+++ src/utils/TestProviders.tsx
@@ -20,10 +20,10 @@ const mockRouter: NextRouter = {
   forward: jest.fn(() => true),
   push: jest.fn(() => Promise.resolve(true)),
   replace: jest.fn(() => Promise.resolve(true)),
-  reload: jest.fn(() => Promise.resolve(true)),
+  reload: jest.fn(() => {}),
   prefetch: jest.fn(() => Promise.resolve()),
-  back: jest.fn(() => Promise.resolve(true)),
-  beforePopState: jest.fn(() => Promise.resolve(true)),
+  back: jest.fn(() => {}),
+  beforePopState: jest.fn(() => {}),
   events: {
     on: jest.fn(),
     off: jest.fn(),
