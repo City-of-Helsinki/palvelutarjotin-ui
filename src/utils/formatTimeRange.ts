6e09d265eda159d8cdd53cfc7e77fdcf717ed05e
Tuomo Kivinen <mrtuomok@gmail.com>
2026-08-26 10:46:38 +0300

    fix: pass locale through formatTimeRange and formatIntoTime
    Refs: PT-2066

--- src/utils/formatTimeRange.ts
+++ src/utils/formatTimeRange.ts
@@ -7,13 +7,11 @@ import { formatIntoTime } from './time/format';
 export default function formatTimeRange(
   start: Date,
   end: Date | null | undefined,
-  // FIXME: Add support for localization, now it's always in Finnish
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   locale: Language
 ): string {
   if (!end) {
-    return formatIntoTime(start);
-  } else {
-    return `${formatIntoTime(start)} – ${formatIntoTime(end)}`;
+    return formatIntoTime(start, locale);
   }
+
+  return `${formatIntoTime(start, locale)} – ${formatIntoTime(end, locale)}`;
 }
